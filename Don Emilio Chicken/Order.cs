3d45db3a50b43068beed6852a8765ccfa613dba1
Daniel Acero <edanielacero@gmail.com>
2020-09-08 11:45:35 -0400

    added order

---
+++ Don Emilio Chicken/Order.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Don_Emilio_Chicken
+{
+    class Order
+    {
+        public string client { get; set; }
+        public int combo { get; set; }
+        public void setClient(string name)
+        {
+            client = name;
+        }
+        public void setCombo(int combon)
+        {
+            combo = combon;
+        }
+    }
+}
